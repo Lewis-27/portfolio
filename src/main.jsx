c502bef263308cc3df358a49ef2beba108d1b1f0
Lewis Miller <lewis@MacBook-Pro-204.lan>
2025-05-22 12:06:43 +0100

    Initialised react router

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import WelcomeScreen from './screens/WelcomeScreen';
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={ <App /> }>
+        <Route index path='/' element={ <WelcomeScreen /> }/> 
+      </Route>
+      )
+  );
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
---
+++ src/screens/WelcomeScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WelcomeScreen = () => {
+  return (
+    <div>
+      welcome
+    </div>
+  )
+}
+
+export default WelcomeScreen
