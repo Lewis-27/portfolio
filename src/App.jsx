5b7780ab71a5f0d47e665c9717d3ca82d6615877
Lewis Miller <lewis@MacBook-Pro-204.lan>
2025-05-29 16:39:53 +0100

    Added adaptive header

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
+import { useState } from 'react'
 
 
 const App = () => {
-  
+  const [showHeader, setShowHeader] = useState(false);
 
   return (
     <>
-      <Header />
-      <Outlet />    
+      <Header showHeader={showHeader} setShowHeader={setShowHeader}/>
+      <div className={`${showHeader ? 'hidden' : 'absolute'} top-0 h-screen w-screen bg-gray-100 opacity-75 z-1` }>
+
+      </div>
+      <Outlet />      
     </>
 
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,36 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { useState } from 'react'
+import { FaBars, FaChevronLeft } from 'react-icons/fa6'
 
-const Header = () => {
+const Header = ({showHeader, setShowHeader}) => {
+  
+
   return (
-    <div className='sticky top-0 w-full h-16 flex items-center justify-center gap-32 bg-white text-lg'>
-      <NavLink className={({isActive}) => isActive ? 'font-bold' : 'font-light'} to={'/'}>Home</NavLink>
-      <NavLink className={({isActive}) => isActive ? 'font-bold' : 'font-light'} to={'/about-me'}>About Me</NavLink>
-      <NavLink className={({isActive}) => isActive ? 'font-bold' : 'font-light'} to={'/projects'}>Projects</NavLink>
-      <NavLink className={({isActive}) => isActive ? 'font-bold' : 'font-light'} to={'/contact'}>Contact</NavLink>
+    <div className='z-50'>
+      <div className='hidden sticky top-0 w-full h-16 md:flex items-center justify-center gap-32 bg-white text-lg'>
+        <NavLink className={({isActive}) => isActive ? 'font-bold' : 'font-light'} to={'/'}>Home</NavLink>
+        <NavLink className={({isActive}) => isActive ? 'font-bold' : 'font-light'} to={'/about-me'}>About Me</NavLink>
+        <NavLink className={({isActive}) => isActive ? 'font-bold' : 'font-light'} to={'/projects'}>Projects</NavLink>
+        <NavLink className={({isActive}) => isActive ? 'font-bold' : 'font-light'} to={'/contact'}>Contact</NavLink>
+      </div>
+
+      <div className="flex absolute md:hidden ">
+        <FaBars className='text-3xl relative top-2 left-2 cursor-pointer z-50' onClick={() => {setShowHeader(!showHeader)}}/>
+        <div className={`flex flex-col h-screen w-32 items-start z-50  ${showHeader ? '-translate-x-40' : '-translate-x-8'} bg-white shadow-lg transition`}>
+          <FaChevronLeft className='cursor-pointer h-8 mx-4 mt-2' onClick={() => {setShowHeader(!showHeader)}}/>
+          <div className="flex flex-col grow items-center justify-evenly w-full ">
+            <NavLink className={({isActive}) => isActive ? 'font-bold' : 'font-light'} to={'/'} onClick={() => {setShowHeader(false)}}>Home</NavLink>
+            <NavLink className={({isActive}) => isActive ? 'font-bold' : 'font-light'} to={'/about-me'} onClick={() => {setShowHeader(false)}}>About Me</NavLink>
+            <NavLink className={({isActive}) => isActive ? 'font-bold' : 'font-light'} to={'/projects'} onClick={() => {setShowHeader(false)}}>Projects</NavLink>
+            <NavLink className={({isActive}) => isActive ? 'font-bold' : 'font-light'} to={'/contact'} onClick={() => {setShowHeader(false)}}>Contact</NavLink>            
+          </div>
+
+        </div>
+      </div>
     </div>
+      
+    
   )
 }
 
